d4b2dd1e336b4fa97061b69571c2393b258f321c
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-04-07 16:45:23 +0200

    Beta 1
    add backup support, push file, camera photos and videos import, reboot, shell
    Beta 1 Ready

--- Start.bat
+++ Start.bat
@@ -148,3 +148,151 @@ echo Select your device
 echo.
 set /p DEVICE= 1- Xiaomi Mi2(s)      2- Xiaomi Mi3    :
 goto :startup
+
+
+
+#############Backup
+
+:backupc
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo 1- Backup       2- Restore
+echo.
+echo 0- Go back
+echo.
+set /p S= ? :
+if %S%==1 goto :bak
+if %S%==2 goto :rest
+if %S%==0 goto :startup
+echo Invalid Input? Try again!...
+pause
+goto :backupc
+
+:bak
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Backup
+echo.
+set /p BAK=Write here your backup name (NO spaces): 
+@adb backup -nosystem -noshared -apk -f C:\XiaomiTool\Backups\%BAK%.ab
+echo Select your password (on phone) if you want, and wait untilt it works.
+pause
+goto :startup
+
+:rest
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Restore
+echo.
+set /p BAK=Write here your backup name (NO spaces): 
+@adb restore C:\XiaomiTool\Backups\%BAK%.ab
+echo Type your password (on phone), and wait untilt it works.
+pause
+goto :startup
+
+
+#############PushandPull
+:sync
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo 1- Push a file   2- Import Photos
+echo.
+echo 0- Go back
+echo.
+set /p S= ? :
+if %S%==1 goto :push
+if %S%==2 goto :camera
+if %S%==0 goto :startup
+echo Invalid Input? Try again!...
+pause
+goto :sync
+
+:push
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Push
+echo.
+set /p PUSH=Drag and drop your file here (one only): 
+@adb push %PUSH& /sdcard/
+pause
+goto :starup
+
+:camera
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Import Photos and Videos
+echo.
+echo File will be placed inside C:\XiaomiTool\Userfiles\Camera
+pause
+@adb pull /sdcard/DCIM/Camera C:\XiaomiTool\Userfiles\
+pause
+goto :starup
+
+
+#############Reboot
+
+:rebootd
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Reboot..
+echo.
+echo 1- System        3- Bootloader
+echo 2- Recovery      4- Download
+echo.
+echo 0- Go back
+echo.
+set /p S= ? :
+if %S%==1 @adb reboot & goto :startup
+if %S%==2 @adb reboot recovery & goto:startup
+if %S%==3 @adb reboot bootloader & goto:startup
+if %S%==4 @adb reboot dload & goto:startup
+if %S%==0 goto :startup
+echo Invalid Input? Try again!...
+pause
+goto :rebootd
+
+
+#############Shell
+:terminal
+cls
+echo ################################
+echo # Xiaomi Toolkit               #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Shell
+echo.
+echo Press CTRL+C when you want to exit from shell
+echo.
+adb shell
+pause
+goto :startup
+
